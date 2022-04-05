6262fa7e89242b3d73cf1d97f5df7b5256d0d558
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-04 21:19:02 -0300

    main

---
+++ len, in, count.py
@@ -0,0 +1,9 @@
+#a variável 'len' vai contar quantos caracteres tem no texto
+#variavel "in" procurando a palavra Python
+
+texto = "aprendendo Python na disciplina de linguagem de programação."
+
+print(f"tamanho do texto ={len(texto)}")
+print(f"Python in texto = {'python' in texto}")
+print(f"Quantidade de y no texto= {texto.count('y')}")
+print(f"as 5 primeiras letras são: {texto [0:5]}")
